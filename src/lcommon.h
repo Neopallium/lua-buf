53587c5d8e74fe6df8dd53d18bf028140bf9fe4e
Robert G. Jakabosky <bobby@sharedrealm.com>
2012-02-26 17:42:26 -0800

    Fix compile errors on windows.

--- src/lcommon.h
+++ src/lcommon.h
@@ -5,19 +5,56 @@
 #if !defined(__L_COMMON_H__)
 #define __L_COMMON_H__
 
-#define L_LIB_API extern
-#define L_INLINE static inline
-
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
 
 #include <assert.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
 #include <sys/types.h>
 #include <sys/user.h>
+
+#ifdef __WINDOWS__
+
+/* for MinGW32 compiler need to include <stdint.h> */
+#ifdef __GNUC__
+#include <stdint.h>
+#include <stdbool.h>
+#else
+
+/* define some standard types missing on Windows. */
+#ifndef __INT32_MAX__
+typedef __int32 int32_t;
+typedef unsigned __int32 uint32_t;
+#endif
+#ifndef __INT64_MAX__
+typedef __int64 int64_t;
+typedef unsigned __int64 uint64_t;
+#endif
+typedef int bool;
+#ifndef true
+#define true 1
+#endif
+#ifndef false
+#define false 0
+#endif
+
+#endif
+
+#define L_LIB_API __declspec(dllexport)
+#define L_INLINE static inline
+
+#else
+
+#define L_LIB_API extern
+#define L_INLINE static inline
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define L_FUNC_UNUSED __attribute__((unused))
+
+#endif
 
 #ifdef NDEBUG
 #define l_assert(expr) do { } while(0)
@@ -44,7 +81,6 @@
 
 #define L_GNUC_PACKED __attribute__((__packed__))
 #define L_UNUSED(var) ((void)var)
-#define L_FUNC_UNUSED __attribute__((unused))
 
 /* L_LIKELY/L_UNLIKELY */
 #if defined(__GNUC__) && (__GNUC__ > 2) && defined(__OPTIMIZE__)
